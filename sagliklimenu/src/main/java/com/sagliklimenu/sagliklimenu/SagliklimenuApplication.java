7c8fcab528011e3c88ed128422aa4099ca66fbf0
yelda.unal <yelda.unal@intra.local>
2019-11-13 13:21:10 +0300

    First commit :)

---
+++ sagliklimenu/src/main/java/com/sagliklimenu/sagliklimenu/SagliklimenuApplication.java
@@ -0,0 +1,13 @@
+package com.sagliklimenu.sagliklimenu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SagliklimenuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SagliklimenuApplication.class, args);
+	}
+
+}
---
+++ sagliklimenu/src/test/java/com/sagliklimenu/sagliklimenu/SagliklimenuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sagliklimenu.sagliklimenu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SagliklimenuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
